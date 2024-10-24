d1f74c0476a5b1cf891ce6fc6b29287b34608fb7
Vyro's Macbook <sheraqib492@gmail.com>
2024-10-24 07:32:18 -0700

    Updated

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-//
+// Mongo DB Connected
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
